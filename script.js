6203370f1410a95baf4168d9f8ae6c9fb9a0efce
Daryl <abrador.daryl@gmail.com>
2020-03-20 17:36:22 +0400

    test

---
+++ script.js
@@ -0,0 +1,24 @@
+let formulaire = document.querySelector('#formulaire');
+let inputField = document.querySelector('#touche');
+
+// formulaire.addEventListener('submit', e =>{
+//     e.preventDefault();
+//     inputField.addEventListener('keydown', e =>{
+//         console.log(e);
+//         console.log(e.key);
+//         console.log(typeof(e.key));
+//         console.log(Number(e.key));
+//         if(e.keyCode === 13){
+//             console.log('tes ouf');
+//         }
+//     })
+// })
+
+// window.onload = function(){
+//     formulaire.click();
+// }
+
+// window.addEventListener("keydown", e =>{
+//     console.log(e.key);
+//     inputField.value += e.key;
+// });
